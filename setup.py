fcdd8ad8bfe773001601cf41a6f786777115b28c
Fatjon Freskina <91606088+fatjonfreskina@users.noreply.github.com>
2023-04-19 09:54:07 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='test-script',
+      version=0.0.1,
+      description='Just print hello world',
+      license='UNIPD',
+      author='Fatjon Freskina',
+      author_email='fatjonfreskina@gmail.com',
+      install_requires=[
+        'pandas', 'numpy'],  # here
+)
